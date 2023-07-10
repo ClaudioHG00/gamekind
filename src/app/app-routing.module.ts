70981c8dc862b1f2ff27a08a3ec497357228a1b8
Claudio Huang <claudiohuang00@gmail.com>
2023-07-10 17:58:27 +0200

    Aggiunto server, riempito il DB di giochi del mock, creata new-game page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,14 @@ import { ErrorComponent } from './components/error/error.component';
 import { DetailComponent } from './components/games/detail/detail.component';
 import { GamesListComponent } from './components/games/games-list/games-list.component';
 import { RegistrationComponent } from './components/user/registration/registration.component';
+import { NewGameComponent } from './components/new-game/new-game.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
   { path: 'games', component: GamesComponent, children: [
     { path: 'detail/:title/:_id', component: DetailComponent},
+    { path: 'new-game', component: NewGameComponent},
     { path: '', component: GamesListComponent, pathMatch:'full'},
   ]},
   { path: 'registration', component: RegistrationComponent},
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { PasswordModule } from 'primeng/password';
 import { DividerModule} from 'primeng/divider';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PaginatorModule } from 'primeng/paginator';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { GameCardComponent } from './shared/game-card/game-card.component';
 import { GamesListComponent } from './components/games/games-list/games-list.component';
 import { DetailComponent } from './components/games/detail/detail.component';
 import { RegistrationComponent } from './components/user/registration/registration.component';
+import { NewGameComponent } from './components/new-game/new-game.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +34,8 @@ import { RegistrationComponent } from './components/user/registration/registrati
     GameCardComponent,
     GamesListComponent,
     DetailComponent,
-    RegistrationComponent
+    RegistrationComponent,
+    NewGameComponent
   ],
   imports: [
     BrowserModule,
@@ -44,6 +47,7 @@ import { RegistrationComponent } from './components/user/registration/registrati
     PasswordModule,
     DividerModule,
     PaginatorModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
